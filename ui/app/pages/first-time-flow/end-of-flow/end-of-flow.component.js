2d124784b0dd5217558cc3fb7c22f508270c3402
Sono Willbrand <sono00@web.de>
2021-06-08 16:50:44 +0200

    working state

--- ui/app/pages/first-time-flow/end-of-flow/end-of-flow.component.js
+++ ui/app/pages/first-time-flow/end-of-flow/end-of-flow.component.js
@@ -68,41 +68,6 @@ export default class EndOfFlowScreen extends PureComponent {
     return (
       <div className="end-of-flow">
         <MetaFoxLogo />
-        <div className="end-of-flow__emoji">🎉</div>
-        <div className="first-time-flow__header">{t('congratulations')}</div>
-        <div className="first-time-flow__text-block end-of-flow__text-1">
-          {t('endOfFlowMessage1')}
-        </div>
-        <div className="first-time-flow__text-block end-of-flow__text-2">
-          {t('endOfFlowMessage2')}
-        </div>
-        <div className="end-of-flow__text-3">
-          {`• ${t('endOfFlowMessage3')}`}
-        </div>
-        <div className="end-of-flow__text-3">
-          {`• ${t('endOfFlowMessage4')}`}
-        </div>
-        <div className="end-of-flow__text-3">
-          {`• ${t('endOfFlowMessage5')}`}
-        </div>
-        <div className="end-of-flow__text-3">
-          {`• ${t('endOfFlowMessage6')}`}
-        </div>
-        <div className="end-of-flow__text-3">
-          {`• ${t('endOfFlowMessage7')}`}
-        </div>
-        <div className="first-time-flow__text-block end-of-flow__text-4">
-          {`*${t('endOfFlowMessage8')}`}&nbsp;
-          <a
-            href="https://metamask.zendesk.com/hc/en-us/articles/360015489591-Basic-Safety-Tips"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="first-time-flow__link-text">
-              {t('endOfFlowMessage9')}
-            </span>
-          </a>
-        </div>
         <Button
           type="primary"
           className="first-time-flow__button"
@@ -110,14 +75,6 @@ export default class EndOfFlowScreen extends PureComponent {
         >
           {t('endOfFlowMessage10')}
         </Button>
-        {onboardingInitiator ? (
-          <Snackbar
-            content={t('onboardingReturnNotice', [
-              t('endOfFlowMessage10'),
-              onboardingInitiator.location,
-            ])}
-          />
-        ) : null}
       </div>
     )
   }
